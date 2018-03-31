0a8104204c6f6560df1510642fc9af308d63c960
Maxim Koltsov <kolmax94@gmail.com>
2018-03-31 19:54:23 +0300

    Fix quoting in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /var/lib/pgadmin/pgadmin4.db ]; then
-    if [ -z ${PGADMIN_SETUP_EMAIL} -o -z ${PGADMIN_SETUP_PASSWORD} ]; then
+    if [ -z "${PGADMIN_SETUP_EMAIL}" -o -z "${PGADMIN_SETUP_PASSWORD}" ]; then
         echo 'You need to specify PGADMIN_SETUP_EMAIL and PGADMIN_SETUP_PASSWORD environment variables'
         exit 1
     fi
@@ -15,7 +15,7 @@ fi
 # Using --threads to have multi-threaded single-process worker
 
 if [ ! -z ${PGADMIN_ENABLE_TLS} ]; then
-    exec gunicorn --bind 0.0.0.0:8080 -w 1 --threads ${GUNICORN_THREADS:-4} --access-logfile - --keyfile /certs/server.key --certfile /certs/server.cert run_pgadmin:app
+    exec gunicorn --bind 0.0.0.0:8080 -w 1 --threads ${GUNICORN_THREADS:-25} --access-logfile - --keyfile /certs/server.key --certfile /certs/server.cert run_pgadmin:app
 else
-    exec gunicorn --bind 0.0.0.0:8080 -w 1 --threads ${GUNICORN_THREADS:-4} --access-logfile - run_pgadmin:app
+    exec gunicorn --bind 0.0.0.0:8080 -w 1 --threads ${GUNICORN_THREADS:-25} --access-logfile - run_pgadmin:app
 fi
